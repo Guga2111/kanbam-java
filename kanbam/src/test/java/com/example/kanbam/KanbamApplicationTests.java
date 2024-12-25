6b7e9bc2f5e9ba24c79d4fde6907ee182e7c9938
Guga2111 <luisgosampaio@gmail.com>
2024-12-25 00:48:37 -0300

    spring initilzr first step

---
+++ kanbam/src/test/java/com/example/kanbam/KanbamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanbam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
