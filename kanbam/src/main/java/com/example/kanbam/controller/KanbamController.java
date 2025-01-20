0ee88e7706d628c4c0fca8b621950138a48b327a
Guga2111 <luisgosampaio@gmail.com>
2025-01-20 11:01:06 -0300

    bug solved

--- kanbam/src/main/java/com/example/kanbam/controller/KanbamController.java
+++ kanbam/src/main/java/com/example/kanbam/controller/KanbamController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -61,7 +62,7 @@ public class KanbamController {
     }
 
     @GetMapping("/task/{id}")
-    public String getTask(Model model, @RequestParam(required = false) String id) {
+    public String getTask(Model model, @PathVariable String id) {
 
         if(id == null) return "redirect:/";
 
