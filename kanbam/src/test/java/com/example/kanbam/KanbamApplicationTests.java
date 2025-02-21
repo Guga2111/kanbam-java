06eecae53343f9985147a7c08a19dd3ad5d9c9dc
luisgustavo <luisgosampaio@gmail.com>
2025-02-21 09:34:41 -0300

    half of integration test implemented

--- kanbam/src/test/java/com/example/kanbam/KanbamApplicationTests.java
+++ kanbam/src/test/java/com/example/kanbam/KanbamApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.kanbam;
 
+import com.example.kanbam.pojo.Priority;
+import com.example.kanbam.pojo.Status;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,8 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -23,6 +30,42 @@ class KanbamApplicationTests {
 	}
 
 	@Test
-	public void testShowTaskForm() throws Exception {
+	public void testShowKanbanBoard() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/");
+
+		mockMvc.perform(request)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(model().attributeExists("tasks"))
+				.andExpect(model().attribute("tasks", (Object) new ArrayList<>()));
+	}
+
+	@Test
+	public void testSubmitTask() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.post("/SubmitTask")
+				.param("name", "Tarefa 1")
+				.param("description", "Tarefa teste")
+				.param("status", Status.TODO.name())
+				.param("priority", Priority.HIGH.name())
+				.param("createDate", LocalDate.now().toString());
+
+		mockMvc.perform(request)
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/"));
+	}
+
+	@Test
+	public void testGetTest() throws Exception{
+		RequestBuilder request = MockMvcRequestBuilders.get("/task/?123");
+
+		mockMvc.perform(request)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(model().attributeExists("task"))
+				.andExpect(view().name("task"));
+	}
+
+	@Test
+	public void testGetKanbanForm() throws Exception{
+		RequestBuilder request = MockMvcRequestBuilders.get("/form");
+
 	}
 }
